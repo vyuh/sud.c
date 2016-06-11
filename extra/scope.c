fd4f783d3d6fd618c8eb15ddc8bbaf161ef20454
Prashant Karmakar <webster15july@gmail.com>
2016-06-11 19:19:58 +0530

    factored sudoku generation

--- extra/scope.c
+++ extra/scope.c
@@ -1,38 +1,59 @@
-#include "../die.c" /* includes stdio.h and stdlib.h */
+#include <stdio.h>
+#include <stdlib.h>
+
+void die(char *msg){
+    fputs(msg, stderr);
+    exit(0);
+}
+
 #include <string.h>
 
-int *scope;
 int main(int argc, char **argv) {
-   int i,j,r,c,gr, gc, n=0,l,s,t; /* n=0 has one purpose */
-   int *target;
-
+   int n=0,p,i, j,l; /* n=0 has one purpose */
+   int *clr;
    if (argc>1) n=atoi(argv[1]);
    if (n<2||n>5) die("usage: only one arg 2,3,4 or 5\n"); 
+   l=n*n*n*n;
+   p=n*n*3-1-n*2;
+   if (!(clr = malloc(l*p*sizeof(int)))) die("no RAM alloted\n");
+   /* malloc from stdlib.h */
+   scope_gen(n, clr);
+   for(i=0;i<l;i++) {
+     for(j=0;j<p;j++) printf("%d ", clr[i*p+j]);
+     puts("");
+   }
+   free(clr);
+   return 0;
+}
+
+int scope_gen(int n, int *clr){
+   int i, j,
+       r, c, gr, gc,
+       l, p;
+   int *eye;
+   unsigned char *scope;
 
    l=n*n*n*n;
-   if (!(scope = malloc(l*sizeof(int)))) die("no RAM alloted\n");  /* malloc from stdlib.h */
-   for (i=0;i<l;i++) {
-      memset(scope, 0, l*sizeof(int)); /* from string.h */
+   if (!(scope = malloc(l*sizeof(unsigned char)))) die("no RAM alloted\n");
+   /* malloc from stdlib.h */
+   for (i=0, eye=clr;i<l;i++) {
+      memset(scope, 0, l*sizeof(unsigned char)); /* from string.h */
       r=i/(n*n);
       c=i%(n*n);
       gr=(r/n)*n;
       gc=(c/n)*n;   
       for (j=0; j<n*n; j++) {
-         t=r*n*n+j;
-         scope[t]++;
-         t=j*n*n+c;
-         scope[t]++;
-         t=(gr+j/n)*n*n+gc+j%n;
-         scope[t]++;
+         scope[r*n*n+j]++;
+         scope[j*n*n+c]++;
+         scope[(gr+j/n)*n*n+gc+j%n]++;
       }
       scope[i]=0;
-      for(j=0; j<l; j++) if (scope[j]) printf("%d ",j); /* from stdio.h */
-      putchar('\n');
+      for(j=0; j<l; j++) if (scope[j]) {*eye = j; eye++;}
    }
+   free(scope);
    return 0;
 }
 /*  generates scope of each cell of sudoku like puzzles
     sudoku has 3 as its fundamental number
     this program accepts 2-5 as the fundamental number
 */
-    
